db971c00227663827b5529438f7605ed8d5ddfe7
konnehmamoud <konnehmamoud@gmail.com>
2025-04-21 10:54:47 -0500

    Added messages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
+import Message from "./components/Message";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All")
   const[favoriteQuotes, setFavoriteQuotes]= useState([])
+  const[messageText, setMessageText] = useState("")
+  const[ showMessage, setShowMessage] = useState(false)
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
@@ -45,16 +48,18 @@ const addToFavorites = (quoteId) => {
   const selectedQuote = quotes.find((quote) => quote.id === quoteId);
 
   const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id);
-  console.log(alreadyFavorite);
+  
 
   if (alreadyFavorite) {
-    console.log("You already favorited this quote!");
+   setMessageText("You already favorited this quote!");
+   setShowMessage(true)
   } else if (favoriteQuotes.length < maxFaves) {
-    console.log("Added to Favorites!");
+    setMessageText("Added to Favorites!");
+    setShowMessage(true)
     setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
   } else {
-    console.log("Max number of favorite quotes reached. Remove one to add another.");
-  }
+    setMessageText("Max number of favorite quotes reached. Remove one to add another.");
+  }   setShowMessage(true)
 };
 
   const removeFromFavorites = (quoteId)=>{
@@ -62,12 +67,16 @@ const updatedFavorites = favoriteQuotes.filter((quote) => quote.id !== quoteId)
 setFavoriteQuotes(updatedFavorites)
   }
   
-// }
+const removeMessage = ()=>{
+  setShowMessage(false)
+}
 
 
   return (
     <div className='App'>
+      {showMessage && <Message messageText={messageText} removeMessage={removeMessage}/> }
       <Header />
+      
       <main>
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites}/>
       
@@ -75,6 +84,7 @@ setFavoriteQuotes(updatedFavorites)
            favoriteQuotes={favoriteQuotes}
           />}
           </main>
+         
       <Footer />
     </div>
   );
